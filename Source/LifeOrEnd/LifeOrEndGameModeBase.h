8d795438160c76cc36892c82e534561106f572a6
DESKTOP-3APC6A8\egagg <egiyazarov.s@gmail.com>
2022-03-20 01:07:35 +0300

    Try again

---
+++ Source/LifeOrEnd/LifeOrEndGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LifeOrEndGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIFEOREND_API ALifeOrEndGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
